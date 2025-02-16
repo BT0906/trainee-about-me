92e702d390aafc53c36552d72da87d25ed3339b5
Brandon Tan <z5478418@ad.unsw.edu.au>
2025-02-16 15:50:47 +1100

    gh-pages setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/trainee-about-me/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
